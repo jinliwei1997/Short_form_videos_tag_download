d3f0858de699f9a0a58e7319941e373b025ace77
Liwei <liwei.jin97@gmail.com>
2020-12-02 19:19:15 +0800

    'v0.1.0'

--- download.py
+++ download.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 import time
 import os
@@ -9,9 +12,23 @@ import requests
 username = 'jinliwei1998'
 password = 'jlw260817'
 
+def wait():
+    try:
+        element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, "myDynamicElement"))
+        )
+    except:
+        pass
 
-def downloadVideo(root,id):
-    url = driver.find_element_by_xpath("//video[1]").get_attribute("src")
+def downloadVideo(link,root,id):
+    driver.get(link)
+    wait()
+    try:
+        url = driver.find_element_by_xpath("//video[1]").get_attribute("src")
+        print('got a video')
+    except:
+        print(f'{link} is not a video.')
+        return False;
     # url = 'https://aweme.snssdk.com/aweme/v1/playwm/?video_id=v0200f9f0000brm8f726tgqapf007a00&ratio=720p&line=0'
     headers = {
         'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36',
@@ -38,24 +55,37 @@ def downloadVideo(root,id):
     print()
 
 def search_tag_and_download_videos(tag = 'cat' , num_videos = 10 ):
+    url = f'https://www.instagram.com/explore/tags/cat/'
+
+    driver.get(url)
+    wait()
+    ls = driver.find_elements_by_xpath("//div[@class='v1Nh3 kIKUG  _bz0w']/a[1]")
+    linklist = []
+    for item in ls:
+        link = item.get_attribute('href')
+        if link not in linklist:
+            linklist.append(link)
+
+    for link in linklist:
+        downloadVideo(link,tag,link.split('/')[-2])
     return
+
+
 
 
 if __name__ == '__main__':
     tag = 'cat'
     login_url = 'https://www.instagram.com/accounts/login/'
     driver = webdriver.Chrome()
+    driver.implicitly_wait(3)
     driver.get(login_url)
-    time.sleep(3)
+
     driver.find_element_by_name('username').send_keys(username)
     driver.find_element_by_name('password').send_keys(password)
     driver.find_element_by_xpath("//button[@class='sqdOP  L3NKy   y3zKF     ']").click()
-    time.sleep(3)
+    wait()
 
-    url = 'https://www.instagram.com/p/CISZ4HUJkxa/'
-    driver.get(url)
-    downloadVideo('cat','CISZ4HUJkxa')
-
+    search_tag_and_download_videos()
 
 
 
